7a5e0bb55f9b5ef105b1f768ee31976859fac960
Ivan Koretskyy <ivan.koretskyy@eleks.com>
2018-10-23 18:36:03 +0300

    add property binding

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    counter: 0
+    counter: this.props.counter
   };
   render() {
     let classes = "badge m-2 badge-";
-    classes += this.state.counter > 0 ? "primary" : "warning";
+    classes += this.state.count > 0 ? "primary" : "warning";
 
     return (
       <div>
+        {this.props.children}
         <span className={classes}>{this.increment()}</span>
         <button
           onClick={this.handleIncrement}
@@ -17,15 +18,21 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.state.counter.id)}
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   handleIncrement = () => {
-    this.setState({ counter: this.state.counter + 1 });
+    this.setState({ count: this.state.count + 1 });
   };
   increment() {
-    return this.state.counter;
+    return this.state.counter.count;
   }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,21 +4,35 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: "1", name: "pc", count: 0 },
-      { id: "2", name: "smartphones", count: 0 },
-      { id: "3", name: "notebook", count: 0 },
-      { id: "4", name: "mp3", count: 0 },
-      { id: "5", name: "toys", count: 0 }
+      { id: "1", name: "pc", count: 4 },
+      { id: "2", name: "smartphones", count: 1 },
+      { id: "3", name: "notebook", count: 3 },
+      { id: "4", name: "mp3", count: 4 },
+      { id: "5", name: "toys", count: 2 }
     ]
   };
   render() {
     return (
       <div>
         {this.state.counters.map(c => (
-          <Counter key={c.id} />
+          <Counter key={c.id} counter={c} onDelete={this.handelDelete}>
+            <p
+              style={{
+                width: 100,
+                display: "inline-block",
+                fontWeight: "bold"
+              }}
+            >
+              {c.name}
+            </p>
+          </Counter>
         ))}
       </div>
     );
+  }
+
+  handelDelete(count) {
+    console.log("handleDelete", count);
   }
 }
 
